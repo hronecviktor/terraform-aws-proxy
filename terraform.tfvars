335ee5914cb1ddc2a914bd600bb996fdb27d509f
Viktor Hronec <zamr666@gmail.com>
2018-02-17 05:46:16 +0100

    Initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+access_key = "<insert>"
+secret_key = "<insert>"
+public_key_file = "<insert path>"
+private_key_file = "<insert path>"
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable access_key {}
+variable secret_key {}
+variable public_key_file {
+  default = "~/.ssh/id_rsa.pub"
+}
+variable private_key_file {
+  default = "~/.ssh/id_rsa"
+}
